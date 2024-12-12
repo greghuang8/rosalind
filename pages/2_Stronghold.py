0c3ed1e995c7972a7a1e8b2f8f7f5dcc8480ad9b
Greg H <greg@gregs-air.lan>
2024-12-12 03:08:24 -0500

    solved first 5 problems

--- pages/2_Stronghold.py
+++ pages/2_Stronghold.py
@@ -1,22 +1,54 @@
 import streamlit as st
 import pandas as pd
+from utils.s_functions import *
 
 st.set_page_config(page_title="Stronghold Solutions", layout="wide")
 st.title("Stronghold Solutions")
 
-num_complete = 0
+num_complete = 5
 st.write(f"Completed Questions: **{str(num_complete)}**/105.")
-st.progress(round(num_complete/105,1), text="Progress:")
+st.progress(round(num_complete/105,2), text="Progress:")
 
 tabs = st.tabs(["1-20", "21-40", "41-60","61-80","81-100","101-105"])
 
 with tabs[0]:
     st.write("Questions 1 to 20:")
-    st.write("Q1")
-    st.write("Q2")
-    st.write("Q3")
-    st.write("Q4")
-    st.write("Q5")
+
+    #Q1
+    with open("data/stronghold/rosalind_dna.txt","r") as f:
+        q1text = f.read()
+    with st.expander("See Q1 Solution"):
+        st.write(q1(q1text))
+
+    #Q2
+    with open("data/stronghold/rosalind_rna.txt","r") as f:
+        q2text = f.read()
+    with st.expander("See Q2 Solution"):
+        st.write(q2(q2text))
+
+    #Q3
+    with open("data/stronghold/rosalind_revc.txt","r") as f:
+        q3text = f.read()
+    with st.expander("See Q3 Solution"):
+        st.write(q3(q3text))
+
+    #Q4
+    with st.expander("See Q4 Solution"):
+        st.write(q4(5,3))
+    #asked 29, 4; answer is 170361678269
+
+    #Q5
+    with st.expander("See Q5 Solution"):
+        df = q5("data/stronghold/rosalind_gc.txt")
+        df["GC-content"] = df["Sequence"].apply(q5_gcCalc)
+        st.write(df)
+        # Find the maximum GC-content
+        max_gc_content = df["GC-content"].max()
+        # Find the name associated with the maximum G-content
+        max_gc_name = df[df["GC-content"] == max_gc_content]["Name"].values[0]
+        st.write(f"Max GC-content Name: {max_gc_name}")
+        st.write(f"GC-content Percentage: {max_gc_content}")
+
     st.write("Q6")
     st.write("Q7")
     st.write("Q8")
