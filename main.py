ee6dbaf6b2f76065e54b1a32bfe82cca5c763cf9
Greg H <greg@Gregs-MacBook-Air.local>
2024-12-11 19:24:08 -0500

    formatting

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 import streamlit as st
 import pandas as pd
 
+# Create an interactive streamlit page to show my solutions to Rosalind questions.
+# Set up main landing page
+st.set_page_config(page_title="Rosalind Solutions", layout="wide")
+st.title("Rosalind Bioinformatics Project")
+
+with open('README.md', 'r') as file:
+    markdown_text = file.read()
+st.markdown(markdown_text)
